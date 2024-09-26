334ed8be68d8b443c64c9441fbb97d78b0527a5b
Stefan Simic <stefansim2000@gmail.com>
2024-09-26 14:07:41 +0200

    add Movie Struct and enable duplicates in value

--- IOS_MODULE_LEARN_APP/ContentView.swift
+++ IOS_MODULE_LEARN_APP/ContentView.swift
@@ -14,14 +14,25 @@ struct ContentView: View {
         } label:  {
             Text("Shuffle")
         }
-        List (movies, id: \.self) { movie in // \ keypath
-                Text("this represents movie \(movie)")
+        List (movies, id: \.id) { movie in // \ keypath, also bei Objekt: self.id bspw.
+            Text("this represents movie \(movie.someMovieData)")
         }.animation(.default, value: movies)
     }
     
     
-    @State var movies = [1, 2, 3, 7, 100, 121, 9999] // Konstante
+    @State var movies: [Movie] = [
+        Movie(someMovieData: 1, id: 1),
+        Movie(someMovieData: 2, id: 2),
+        Movie(someMovieData: 3, id: 3),
+        Movie(someMovieData: 3, id: 4),
+        Movie(someMovieData: 4, id: 5),
+    ] // Konstante
     
+}
+
+struct Movie: Equatable {
+    let someMovieData: Int
+    let id: Int
 }
 
 #Preview {
