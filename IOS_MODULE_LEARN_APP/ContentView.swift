1359e09687a37d89532c0edd9ce837e85412245f
Stefan Simic <stefansim2000@gmail.com>
2024-10-10 13:06:38 +0200

    week 03

--- IOS_MODULE_LEARN_APP/ContentView.swift
+++ IOS_MODULE_LEARN_APP/ContentView.swift
@@ -10,37 +10,50 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         List (movies, id: \.id) { movie in // \ keypath, also bei Objekt: self.id bspw.
-            Text("this represents movie \(movie.title)")
+            Text(movie.title)
         }.refreshable {
-            print("Refresh!")
-            movies = downloadMovies()
+            await refresh()
+        }.task {
+            await refresh()
         }
     }
     
+    func refresh() async {
+        movies = await downloadMovies()
+    }
     
-    @State var movies: [Movie] = [
-        Movie(title: "movieTitle 1"),
-        Movie(title: "movieTitle 2"),
-        Movie(title: "movieTitle 3"),
-        Movie(title: "movieTitle 3"),
-        Movie(title: "movieTitle 4"),
-    ] // Konstante
+    @State var movies = [Movie]()// Konstante
     
 }
 
-func downloadMovies() -> [Movie] {
+func downloadMovies() async -> [Movie] {
     // TODO: Download movies
-    let apiKey = "???"
-    let urlString =
+    //let apiKey = "???"
+    // let urlString = "???"
     
+    // Schamlos geklaut vom Lehrer Sebastian Fichtner:
+    let apiKey = "59c98f37425372d96f2137ad9e981bd5"
+    let urlString = "https://api.themoviedb.org/3/movie/popular?api_key=\(apiKey)"
     
+    guard let url = URL(string: urlString) else {
+        print("Error: could not create URL from String: \(urlString)")
+        return []
+    }
+
+    let (data, _) = try! await URLSession.shared.data(from: url)
+    let tmdbResponse = try! JSONDecoder().decode(TMDBResponse.self, from: data)
     
-    return []
+    return tmdbResponse.results
 }
 
-struct Movie: Equatable {
+struct TMDBResponse: Decodable {
+    let results: [Movie]
+}
+
+struct Movie: Identifiable, Equatable, Decodable {
     let title: String
-    let id = UUID()
+    let id: Int
+    let poster_path: String
 }
 
 #Preview {
